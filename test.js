8505b92614cd4324527fb7af0517f68493e3cc12
Colin <colin.diesh@gmail.com>
2021-08-18 20:06:02 -0400

    Use createGunzip

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 import fs from "fs";
 import esort from "external-sort";
+import { createGunzip } from "zlib";
 
 console.log("processing", process.argv[2], process.argv[3]);
 await esort(
-  fs.createReadStream(process.argv[2]),
+  fs.createReadStream(process.argv[2]).pipe(createGunzip()),
   fs.createWriteStream(process.argv[3]),
   {
     maxHeap: 1024 * 1024,
--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-gunzip test1.txt.gz
-gunzip test2.txt.gz
-node test.js test1.txt test1.sorted.txt
-node test.js test2.txt test2.sorted.txt
+node test.js test1.txt.gz test1.sorted.txt
+node test.js test2.txt.gz test2.sorted.txt
